182698aa3af3ec2863654d9cc7d651634f7e8cfc
Giovanni Belfiore <g.belfiore@doveconviene.com>
2023-11-06 11:01:36 +0100

    wip 100

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,9 @@ import Layout from "@/components/layout/Layout";
 import { useCallback } from "react";
 
 const Home = () => {
-	const xxx = useCallback(async () => {
-		let data = await getAllDay();
-		console.log(data);
-	}, []);
 	return (
 		<Layout title="Home Page">
-			<div>cisoooo</div>
+			<div>Home Page: coming soon - click on users or days</div>
 		</Layout>
 	);
 };
