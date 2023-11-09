5a136005644aad916e86ea0e60498a045294d732
Giovanni Belfiore <g.belfiore@doveconviene.com>
2023-11-09 13:50:45 +0100

    wip xxx

--- src/components/manage-user/ManageUser.tsx
+++ src/components/manage-user/ManageUser.tsx
@@ -6,6 +6,7 @@ import styles from "./ManageUser.module.css";
 import FormUser from "../form-user/FormUser";
 import classNames from "classnames";
 import useModalState from "@/zustand/modalState";
+import { fantaMoneyToEuro } from "@/utils/MoneyUtils";
 
 const ManageUser = () => {
 	const [users, setUsers] = useState<Array<IUser> | null>(null);
@@ -77,12 +78,12 @@ const ManageUser = () => {
 					<tr>
 						<th className="w-[100px]">ID</th>
 						<td className="w-[250px]">Nome</td>
-						<td className="w-[250px]">Cognome</td>
-						<td className="w-[250px]"># Partite</td>
-						<td className="w-[250px]">Money In</td>
-						<td className="w-[250px]">Money Out</td>
-						<td className="w-[250px]">Andamento giocatore</td>
-						<th></th>
+						<td className="w-[150px]">Cognome</td>
+						<td className="w-[100px] text-center"># Partite</td>
+						<td className="w-[150px] text-center">Andamento Money In</td>
+						<td className="w-[150px] text-center">Andamento Money Out</td>
+						<td className="w-[150px] text-center">Andamento esito</td>
+						<th className="w-[150px]"></th>
 					</tr>
 				</thead>
 				<tbody>
@@ -94,11 +95,24 @@ const ManageUser = () => {
 								<th>{user.id}</th>
 								<td>{user.name}</td>
 								<td>{user.surname}</td>
-								<td>{user.userDay?.length}</td>
-								<td>{moneyIn}</td>
-								<td>{moneyOut}</td>
-								<td className={classNames({ "text-green-600": moneyOut - moneyIn > 0, "text-red-600": moneyOut - moneyIn < 0 })}>
-									{moneyOut - moneyIn}
+								<td className="text-center">{user.userDay?.length}</td>
+								<td>
+									<div className="flex flex-col gap-2 items-center">
+										<span className="text-orange-500">{`${moneyIn}¥`}</span>
+										<span className="text-violet-500">{`${fantaMoneyToEuro(moneyIn)}€`}</span>
+									</div>
+								</td>
+								<td>
+									<div className="flex flex-col gap-2 items-center">
+										<span className="text-orange-500">{`${moneyOut}¥`}</span>
+										<span className="text-violet-500">{`${fantaMoneyToEuro(moneyOut)}€`}</span>
+									</div>
+								</td>
+								<td className={classNames({ "bg-green-200": moneyOut - moneyIn > 0, "bg-red-200": moneyOut - moneyIn < 0 })}>
+									<div className="flex flex-col gap-2 items-center">
+										<span className="text-orange-500">{`${moneyOut - moneyIn}¥`}</span>
+										<span className="text-violet-500">{`${fantaMoneyToEuro(moneyOut - moneyIn)}€`}</span>
+									</div>
 								</td>
 								<td className="flex flex-col gap-2 justify-end">
 									<button className="btn btn-info btn-sm" onClick={() => onEditUser(user)}>
